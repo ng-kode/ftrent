77567b74378ba7de436523e7cca22f3310f829be
Richard Ng <richard.ng.cccc@gmail.com>
2021-04-19 15:50:33 +0800

    mutate data --> datasets

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,50 @@ import Chart from './Chart';
 
 function App() {
   const myChart = useRef(null);
-  const [data, setData] = useState({
-    datasets: [
-      {
-        label: "Scatter Dataset",
-        data: [
-          {
-            x: -10,
-            y: 0,
-          },
-          {
-            x: 0,
-            y: 10,
-          },
-          {
-            x: 10,
-            y: 5,
-          },
-          {
-            x: 0.5,
-            y: 5.5,
-          },
-        ],
-        backgroundColor: "rgb(255, 99, 132)",
-      },
-    ],
-  });
+  const [datasets, setDatasets] = useState([
+    {
+      label: "Scatter Dataset",
+      data: [
+        {
+          x: -10,
+          y: 0,
+        },
+        {
+          x: 0,
+          y: 10,
+        },
+        {
+          x: 10,
+          y: 5,
+        },
+        {
+          x: 0.5,
+          y: 5.5,
+        },
+      ],
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+  ]);
 
   useEffect(() => {
     if (!myChart.current) {
       console.log('useEffect init Chart');
       const ctx = document.getElementById('myChart');
-      const config = {
+      myChart.current = new Chart(ctx, {
         type: "scatter",
-        data: data,
+        data: { datasets },
         options: {},
-      };
-      myChart.current = new Chart(ctx, config);
+      });
     } else {
-      console.log('useEffect update Chart', data);
-      myChart.current.data = data;
+      console.log('useEffect update Chart');
+      myChart.current.data = { datasets };
       myChart.current.update();
     }
-  }, [data]);
+  }, [datasets]);
 
   const randGen = () => {
     const newData = Array(6).fill(null).map(() => ({x: Math.random(), y: Math.random()}));
-    setData({ datasets: [ { ...data.datasets[0], data: newData } ] });
+    setDatasets([ { ...datasets[0], data: newData } ]);
   }
 
   return (
@@ -68,7 +65,7 @@ function App() {
       <button onClick={randGen}>RandGen</button>
 
       <pre>
-        {JSON.stringify(data, null, 2)}
+        {JSON.stringify(datasets, null, 2)}
       </pre>
     </div>
   );
