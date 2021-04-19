e784bc651bb5e229c5c7cc410114aba1137a6eff
Richard Ng <richard.ng.cccc@gmail.com>
2021-04-19 15:41:18 +0800

    chartjs dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,56 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Chart from './Chart';
 
 function App() {
+  const myChart = useRef(null);
+  const [data, setData] = useState({
+    datasets: [
+      {
+        label: "Scatter Dataset",
+        data: [
+          {
+            x: -10,
+            y: 0,
+          },
+          {
+            x: 0,
+            y: 10,
+          },
+          {
+            x: 10,
+            y: 5,
+          },
+          {
+            x: 0.5,
+            y: 5.5,
+          },
+        ],
+        backgroundColor: "rgb(255, 99, 132)",
+      },
+    ],
+  });
 
   useEffect(() => {
-    const data = {
-      datasets: [
-        {
-          label: "Scatter Dataset",
-          data: [
-            {
-              x: -10,
-              y: 0,
-            },
-            {
-              x: 0,
-              y: 10,
-            },
-            {
-              x: 10,
-              y: 5,
-            },
-            {
-              x: 0.5,
-              y: 5.5,
-            },
-          ],
-          backgroundColor: "rgb(255, 99, 132)",
-        },
-      ],
-    };
+    if (!myChart.current) {
+      console.log('useEffect init Chart');
+      const ctx = document.getElementById('myChart');
+      const config = {
+        type: "scatter",
+        data: data,
+        options: {},
+      };
+      myChart.current = new Chart(ctx, config);
+    } else {
+      console.log('useEffect update Chart', data);
+      myChart.current.data = data;
+      myChart.current.update();
+    }
+  }, [data]);
 
-    const config = {
-      type: "scatter",
-      data: data,
-      options: {},
-    };
-
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, config);
-  }, []);
+  const randGen = () => {
+    const newData = Array(6).fill(null).map(() => ({x: Math.random(), y: Math.random()}));
+    setData({ datasets: [ { ...data.datasets[0], data: newData } ] });
+  }
 
   return (
     <div>
@@ -53,6 +64,12 @@ function App() {
       >
         <canvas id="myChart"></canvas>
       </div>
+
+      <button onClick={randGen}>RandGen</button>
+
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </div>
   );
 }
